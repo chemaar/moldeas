e14a21d7e28684c53830bc2caae62c680f5f6387
Jose María Alvarez <jmalvarez@seerc.org>
2013-02-01 13:14:20 +0200

    Moving moldeas-api

---
+++ apps/moldeas-api/src/main/java/org/weso/moldeas/enhancers/NLPEnhancer.java
@@ -0,0 +1,23 @@
+package org.weso.moldeas.enhancers;
+
+public class NLPEnhancer extends EnhancerAdapter {
+
+	
+	protected void execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	protected void preExecute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	protected void postExecute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
